1c06c67004395476d2f2c5c5fa5e159b58f3438c
Ansh Kumar Gupta <63894658+Ansh7899@users.noreply.github.com>
2021-03-29 22:49:33 +0530

    Delete main.py

--- .gitignore/main.py
+++
@@ -1,7 +0,0 @@
-from translate import Translator
-lang1 = input("Enter the language of text:")
-lang2 = input("Enter the language ou want to convert the text in:")
-translator = Translator(from_lang=lang1,to_lang=lang2)
-stringInput = input("Enter the text you want to translate:")
-translation = translator.translate(stringInput)
-print(translation)
